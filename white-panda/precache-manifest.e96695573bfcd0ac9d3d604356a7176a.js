04db4825648e335994890c22748f4fd826ec238a
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-10-10 05:52:39 +0530

    white-panda-production

---
+++ white-panda/precache-manifest.e96695573bfcd0ac9d3d604356a7176a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68de1db1999d42829ef2056e11739ec0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44a483507e8f258f6042",
+    "url": "./static/css/main.b54172a1.chunk.css"
+  },
+  {
+    "revision": "8dbbba2ce4bb6b16b84f",
+    "url": "./static/js/2.7e1157f6.chunk.js"
+  },
+  {
+    "revision": "44a483507e8f258f6042",
+    "url": "./static/js/main.c6439e81.chunk.js"
+  },
+  {
+    "revision": "e2a15bd302e6ac3efdf7",
+    "url": "./static/js/runtime-main.566f7937.js"
+  }
+]);
