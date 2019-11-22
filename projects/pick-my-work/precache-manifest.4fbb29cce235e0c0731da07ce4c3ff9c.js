ab234543372fc4b7ad3b380b492e5e524bf843ab
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-11-22 11:34:58 +0530

    pick-my-work

---
+++ projects/pick-my-work/precache-manifest.4fbb29cce235e0c0731da07ce4c3ff9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b0709b7648b9f62ac6b90af2317a61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fc7bd4f4f71a232b0b9",
+    "url": "./static/css/2.66e7b8c9.chunk.css"
+  },
+  {
+    "revision": "a72e3312fcbb7056c739",
+    "url": "./static/css/main.1be2664f.chunk.css"
+  },
+  {
+    "revision": "2fc7bd4f4f71a232b0b9",
+    "url": "./static/js/2.3296ab03.chunk.js"
+  },
+  {
+    "revision": "a72e3312fcbb7056c739",
+    "url": "./static/js/main.d8a61914.chunk.js"
+  },
+  {
+    "revision": "ef15f0c2c006ebcb84ad",
+    "url": "./static/js/runtime-main.581889c1.js"
+  }
+]);
