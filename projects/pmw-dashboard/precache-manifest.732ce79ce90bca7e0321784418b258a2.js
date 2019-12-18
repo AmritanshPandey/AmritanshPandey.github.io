056b58089df695023102a6259830482d369a483f
Amritansh <amritansh.p@reculta.com>
2019-12-18 09:01:35 +0530

    added new project

---
+++ projects/pmw-dashboard/precache-manifest.732ce79ce90bca7e0321784418b258a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1666229b8bae2f30983981e944030ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b89f0432ea8bee5261b",
+    "url": "./static/css/main.4f7bd742.chunk.css"
+  },
+  {
+    "revision": "c5d6506233deb5a16670",
+    "url": "./static/js/2.be76d6f5.chunk.js"
+  },
+  {
+    "revision": "fed0ffe46930953d9d68ec09145dd749",
+    "url": "./static/js/2.be76d6f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b89f0432ea8bee5261b",
+    "url": "./static/js/main.0682f7ff.chunk.js"
+  },
+  {
+    "revision": "af88f608b1cd0c474413",
+    "url": "./static/js/runtime-main.448672d0.js"
+  },
+  {
+    "revision": "42ddde848c4e789f3661a28f326edb05",
+    "url": "./static/media/Flaticon.42ddde84.ttf"
+  },
+  {
+    "revision": "5ab2308e8bd4e0206ab5b9bc64e1e317",
+    "url": "./static/media/Flaticon.5ab2308e.woff"
+  },
+  {
+    "revision": "6c61b057da7a76c0226b7efd0c3cbd13",
+    "url": "./static/media/Flaticon.6c61b057.eot"
+  },
+  {
+    "revision": "dfb2f7d694098c6735626fc1238e9dda",
+    "url": "./static/media/Flaticon.dfb2f7d6.woff2"
+  },
+  {
+    "revision": "e35511e2537294df198cc8728a7f17fe",
+    "url": "./static/media/Flaticon.e35511e2.svg"
+  },
+  {
+    "revision": "61ece80e6de71bf6c460edcbfa865fb1",
+    "url": "./static/media/login.61ece80e.png"
+  }
+]);
